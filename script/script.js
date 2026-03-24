9d501be827315793a23f5494e80c0d8e56feb08a
dianaparagina90-ai <diana.paragina90@gmail.com>
2026-03-24 15:26:43 +0100

    last details

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
 const slides = document.querySelectorAll(".carousel img");
+const toggle= document.querySelector(".menu-toggle");
+const navbar=document.querySelector(".navbar");
 
 let index =0;
 
@@ -6,4 +8,8 @@ setInterval(()=>{
     slides[index].classList.remove("active");
     index = (index+1) % slides.length;
     slides[index].classList.add("active")
-},4000)
+},4000);
+
+toggle.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+});
