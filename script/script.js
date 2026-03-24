e4d953cb5d4350ece13d0ac75552609044b929bc
dianaparagina90-ai <diana.paragina90@gmail.com>
2026-03-24 14:07:02 +0100

    add styling in right map and complete with picture slides

---
+++ script/script.js
@@ -0,0 +1,9 @@
+const slides = document.querySelectorAll(".carousel img");
+
+let index =0;
+
+setInterval(()=>{
+    slides[index].classList.remove("active");
+    index = (index+1) % slides.length;
+    slides[index].classList.add("active")
+},4000)
